faff7837787cf180b581f4b2d69a7654d85173e4
thinkcodeforlife <ermac_scorpion@hotmail.com>
2020-07-14 23:58:54 +0300

    Add vue files about projects

---
+++ src/components/Project.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h3>{{ project.title }}</h3>
+        <span>{{ formatted_date }}</span>
+        <p>{{ project.details }}</p>
+        <ul>
+            <li v-for="(item, x) in project.stack" :key="x">
+                <div>{{ item }}</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+    // import { mapState } from 'vuex';
+
+    export default {
+        name: "Project",
+        props: ["project"],
+        data: () => ({
+            months: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            ],
+            // formatted_date: this.months[new Date(this.$props.project.date).getMonth()]
+        }),
+        computed: {
+            formatted_date() {
+                return  this.months[new Date(this.$props.project.date).getMonth()] + " " + new Date(this.$props.project.date).getFullYear()
+            }
+        }
+    }
+
+</script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Projects</h1>
+        <hr>
+        <ul>
+            <li v-for="(project, x) in projects" :key="x">
+                <Project v-bind:project="project" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+    import Project from "./Project.vue";
+
+    // import { mapState } from 'vuex';
+
+    export default {
+        name: "Projects",
+        props: ["projects"],
+        components: {
+            Project
+        },
+    }
+
+</script>
